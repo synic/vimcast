a0de923b4b96b8bfc952f82ef02e532f82911a8b
Adam Olsen <arolsen@gmail.com>
2015-08-14 07:37:51 -0600

    finished macro demo file

--- users/utils.py
+++ users/utils.py
@@ -1,10 +1,35 @@
 from somelibrary import somefunction
 
+
 def calculate_stuff():
     """
     Just to demonstrate macros - `somefunction` accepts a dictionary, not a
     list of variables, so we need to convert the following variable
     definitions to a dictionary.
+
+    To record the macro to change these lines to a dictionary definition:
+
+    Put the cursor on the first line
+    1.  Press qa      - starts recording in register `a`
+    2.  Press ^       - go to the beginning of the current line
+    3.  Press i"      - go to insert mode and insert a "
+    4.  Press Escape  - exit insert mode
+    5.  Press ww      - jump forward the next two "words"
+    6.  Press h       - move back one character
+    7.  Press i"      - go to insert mode and insert a "
+    8.  Press Escape  - exit insert mode
+    9.  Press l       - move right one character
+    10. Press x       - delete the current character
+    11. Press r:      - replace the current character with a :
+    12. Press A,      - move the cursor to the end of the line, enter insert
+                        mode, and insert a comma
+    13. Press Escape  - exit insert mode
+    14. Press j       - move down one line
+    15. Press q       - stop recording
+
+    Now to run the macro on the next line, type @a (which means, "run the
+    macro in register `a`").
+
     """
     first_var = "somestuff"
     second_var = "someotherstuff"
